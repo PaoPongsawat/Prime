b3a5e59bf8ebdcc632cc48c965d919e83aff42b6
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-27 12:09:40 +0700

    New

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -9,6 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Impricit
+    let strAlertHaveSpace = "Please Fill Number"
+    let strAlerNumberFalse = "Palase Number Onry"
+    
+    
+    
+    
+    @IBOutlet weak var NumberTextField: UITextField!
+    
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    
+    @IBAction func checkButton(_ sender: Any) {
+        
+        let strNameberFromTextField = NumberTextField.text!
+        print("Number ==> \(strNameberFromTextField)")
+        
+        
+        //Check Space
+        if strNameberFromTextField != "" {
+            print("No Sapce")
+            
+            //Check Number or Artical
+            
+            if let intNumberFromTextField = Int(strNameberFromTextField) {
+                print("Thid is Number")
+                
+            }else{
+                print("This is Arical")
+                resultLable.text = strAlerNumberFalse
+            }
+            
+            
+            
+            
+        }else{
+            print("Have Space")
+            resultLable.text = strAlertHaveSpace
+        }
+        
+        
+    } //CheckButton
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ README.md
@@ -0,0 +1,4 @@
+# แอพค้นหาจำนวนเฉพาะ
+## For Learn Optional
+
+[![Screen Shot 2560-07-27 at 11.50.21 AM.png](https://s12.postimg.org/q60e14enh/Screen_Shot_2560-07-27_at_11.50.21_AM.png)](https://postimg.org/image/u2dpx3zmx/)
